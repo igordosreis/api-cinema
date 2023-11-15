4da881447722bbdf45fb2d24199e379e49e3d74e
Igor dos Reis <igorreis@gmail.com>
2023-11-15 16:08:45 -0300

    feat: add establishment_images migration

---
+++ src/database/migrations/create-establishments_images.js
@@ -0,0 +1,36 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable strict */
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('establishment_images', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      image: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      image_carousel: {
+        type: Sequelize.STRING(255),
+        defaultValue: null,
+      },
+      cover: {
+        type: Sequelize.STRING(120),
+        defaultValue: null,
+      },
+      resize_color: {
+        type: Sequelize.STRING(255),
+        defaultValue: '#ffffff',
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('establishment_images');
+  },
+};
