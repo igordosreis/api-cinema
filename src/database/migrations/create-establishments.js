fbdafb5ec41080874c8ad7944a69a9bedb7443c7
Igor dos Reis <igorreis@gmail.com>
2023-11-15 16:02:39 -0300

    feat: add establishments migration

--- src/database/migrations/create-establishments.js
+++ src/database/migrations/create-establishments.js
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable max-lines-per-function */
 /* eslint-disable strict */
 
@@ -25,24 +24,77 @@ module.exports = {
       },
       brand: {
         type: Sequelize.STRING,
-        allowNull: true,
-        collate: 'utf8mb3_unicode_ci',
+        characterSet: 'utf8mb3',
       },
       primary_color: {
         type: Sequelize.STRING,
-        allowNull: true,
         defaultValue: '000000',
+        allowNull: false,
+        characterSet: 'utf8mb3',
         collate: 'utf8mb3_unicode_ci',
       },
       link: {
         type: Sequelize.TEXT('long'),
-        allowNull: true,
         collate: 'utf8mb3_unicode_ci',
       },
       link_description: {
-        type: Sequelize.STRING,
-        allowNull: true,
+        type: Sequelize.STRING(80),
+        allowNull: false,
         collate: 'utf8mb3_unicode_ci',
+      },
+      telephone: {
+        type: Sequelize.STRING(150),
+        collate: 'utf8mb3_unicode_ci',
+        defaultValue: null,
+      },
+      telephone_2: {
+        type: Sequelize.STRING(150),
+        collate: 'utf8mb3_unicode_ci',
+        defaultValue: null,
+      },
+      whatsapp: {
+        type: Sequelize.STRING(150),
+        collate: 'utf8mb3_unicode_ci',
+        defaultValue: null,
+      },
+      instagram: {
+        type: Sequelize.STRING(150),
+        collate: 'utf8mb3_unicode_ci',
+        defaultValue: null,
+      },
+      site: {
+        type: Sequelize.STRING(150),
+        collate: 'utf8mb3_unicode_ci',
+        defaultValue: null,
+      },
+      rules: {
+        type: Sequelize.TEXT('long'),
+        allowNull: false,
+        collate: 'utf8mb3_unicode_ci',
+      },
+      key_words: {
+        type: Sequelize.TEXT,
+        collate: 'utf8mb3_unicode_ci',
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      views: {
+        type: Sequelize.BIGINT,
+        defaultValue: 0,
+      },
+      under_highlight: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
       },
     }, {
       underscored: true,
