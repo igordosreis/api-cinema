67ca18e17172b1817d8a5ce6cd6ef6b78de83f25
igordosreis <igorreis@gmail.com>
2023-11-16 10:38:54 -0500

    feat: add states seeder

---
+++ src/database/seeders/01-seed-states.js
@@ -0,0 +1,144 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable max-len */
+/* eslint-disable max-lines-per-function */
+
+module.exports = {
+  up: async (QueryInterface) => {
+    await QueryInterface.bulkInsert('states', [
+      {
+        id: 0,
+        name: 'Todos',
+        abbreviation: null,
+      },
+      {
+        id: 1,
+        name: 'Acre',
+        abbreviation: 'AC',
+      },
+      {
+        id: 2,
+        name: 'Alagoas',
+        abbreviation: 'AL',
+      },
+      {
+        id: 3,
+        name: 'Amapá',
+        abbreviation: 'AP',
+      },
+      {
+        id: 4,
+        name: 'Amazonas',
+        abbreviation: 'AM',
+      },
+      {
+        id: 5,
+        name: 'Bahia',
+        abbreviation: 'BA',
+      },
+      {
+        id: 6,
+        name: 'Ceará',
+        abbreviation: 'CE',
+      },
+      {
+        id: 7,
+        name: 'Distrito Federal',
+        abbreviation: 'DF',
+      },
+      {
+        id: 8,
+        name: 'Espiríto Santo',
+        abbreviation: 'ES',
+      },
+      {
+        id: 9,
+        name: 'Goiás',
+        abbreviation: 'GO',
+      },
+      {
+        id: 10,
+        name: 'Maranhão',
+        abbreviation: 'MA',
+      },
+      {
+        id: 11,
+        name: 'Mato Grosso',
+        abbreviation: 'MT',
+      },
+      {
+        id: 12,
+        name: 'Mato Grosso do Sul',
+        abbreviation: 'MS',
+      },
+      {
+        id: 13,
+        name: 'Minas Gerais',
+        abbreviation: 'MG',
+      },
+      {
+        id: 14,
+        name: 'Pará',
+        abbreviation: 'PA',
+      },
+      {
+        id: 15,
+        name: 'Paraíba',
+        abbreviation: 'PB',
+      },
+      {
+        id: 16,
+        name: 'Paraná',
+        abbreviation: 'PR',
+      },
+      {
+        id: 17,
+        name: 'Pernambuco',
+        abbreviation: 'PE',
+      },
+      {
+        id: 18,
+        name: 'Piauí',
+        abbreviation: 'PI',
+      },
+      {
+        id: 19,
+        name: 'Rio de Janeiro',
+        abbreviation: 'RJ',
+      },
+      {
+        id: 20,
+        name: 'Rio Grande do Norte',
+        abbreviation: 'RN',
+      },
+      {
+        id: 21,
+        name: 'Rio Grande do Sul',
+        abbreviation: 'RS',
+      },
+      {
+        id: 22,
+        name: 'Rondônia',
+        abbreviation: 'RO',
+      },
+      {
+        id: 23,
+        name: 'Roraima',
+        abbreviation: 'RR',
+      },
+      {
+        id: 24,
+        name: 'Santa Catarina',
+        abbreviation: 'SC',
+      },
+      {
+        id: 25,
+        name: 'São Paulo',
+        abbreviation: 'SP',
+      },
+    ], {});
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('establishments', null, {});
+  },
+};
