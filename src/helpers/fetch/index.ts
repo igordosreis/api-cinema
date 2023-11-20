71ddbb60a9880abc1df34bc62cd06fc1dd175784
igordosreis <igorreis@gmail.com>
2023-11-20 14:43:16 -0500

    feat: getPopular fetch

--- src/helpers/fetch/index.ts
+++ src/helpers/fetch/index.ts
@@ -1,14 +1,15 @@
+/* eslint-disable max-len */
 import axios from 'axios';
 import { IDate } from '../../interfaces/IUrlDate';
-import { RELEASE_DATE_END, RELEASE_DATE_START, URL_BASE } from '../../constants';
+import {
+  RELEASE_DATE_END,
+  RELEASE_DATE_START,
+  SORT_BY_POPULARITY,
+  URL_BASE,
+} from '../../constants';
 import { IMovieAPIResponse } from '../../interfaces/IMoviesAPI';
 
-export const getNowPlaying = async (
-  currentDate: IDate,
-  pastDate: IDate,
-): Promise<IMovieAPIResponse | undefined> => {
-  const endpoint = `${URL_BASE}${RELEASE_DATE_START}${pastDate}${RELEASE_DATE_END}${currentDate}`;
-
+const fetch = async (endpoint: string): Promise<IMovieAPIResponse | undefined> => {
   try {
     const { data } = await axios.get<IMovieAPIResponse>(endpoint);
 
@@ -18,6 +19,20 @@ export const getNowPlaying = async (
   }
 };
 
-export const getPopular = async (_currentDate: IDate, _pastDate: IDate) => {};
+export const getNowPlaying = async (currentDate: IDate, pastDate: IDate) => {
+  const endpoint = `${URL_BASE}${RELEASE_DATE_START}${pastDate}${RELEASE_DATE_END}${currentDate}`;
+
+  const data = await fetch(endpoint);
+
+  return data;
+};
+
+export const getPopular = async (currentDate: IDate, pastDate: IDate) => {
+  const endpoint = `${URL_BASE}${RELEASE_DATE_START}${pastDate}${RELEASE_DATE_END}${currentDate}${SORT_BY_POPULARITY}`;
+
+  const data = await fetch(endpoint);
+
+  return data;
+};
 // export const getUpcoming = async (currentDate: IDate, futureDate: IDate) => {};
 // export const getPremier = async (previousSundayDate: IDate, nextSundayDate: IDate) => {};
