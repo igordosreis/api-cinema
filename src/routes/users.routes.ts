509e17f1038b4d75bb1e1b8c40857c724f833f07
Igor dos Reis <igorreis@gmail.com>
2023-12-08 15:40:41 -0300

    feat: add cancelOrder route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -5,7 +5,8 @@ const usersRouter = Router();
 
 usersRouter.get('/orders/all', UsersController.getAllOrders);
 usersRouter.post('/orders/create', UsersController.createOrder);
-usersRouter.get('/orders/search', UsersController.getOrderById);
+usersRouter.get('/orders/details', UsersController.getOrderById);
+usersRouter.post('/orders/details', UsersController.cancelOrder);
 
 // PARA TESTE
 usersRouter.get('/vouchers_product', UsersController.getVouchersByProductId);
