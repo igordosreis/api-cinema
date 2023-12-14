44b9949181ab0738f76c9f846ca76fd369bdd873
igordosreis <igorreis@gmail.com>
2023-12-14 11:47:13 -0300

    feat: add products_types migration

---
+++ src/database/migrations/12-create-products_types.js
@@ -0,0 +1,42 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable strict */
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('products_types', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        collate: 'utf8mb3_unicode_ci',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      expire_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+    }, {
+      underscored: true,
+      timestamps: false,
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('products_types');
+  },
+};
