bc89e148fe2c9bea820abb5ced29478e5d495b1d
igordosreis <igorreis@gmail.com>
2023-12-14 11:09:52 -0300

    feat: add packs migration

---
+++ src/database/migrations/12-create-packs.js
@@ -0,0 +1,76 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable strict */
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('packs', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        collate: 'utf8mb3_unicode_ci',
+      },
+      description: {
+        type: Sequelize.TEXT('long'),
+        allowNull: false,
+        collate: 'utf8mb3_unicode_ci',
+      },
+      image: {
+        type: Sequelize.STRING,
+        defaultValue: null,
+      },
+      price: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      rules: {
+        type: Sequelize.TEXT('long'),
+        allowNull: false,
+        collate: 'utf8mb3_unicode_ci',
+      },
+      counter: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      counterLimit: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      isLimited: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      expire_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+        defaultValue: null,
+      },
+    }, {
+      underscored: true,
+      timestamps: false,
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('packs');
+  },
+};
