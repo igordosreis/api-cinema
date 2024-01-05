4958bd4bb20e33f0da78d0efa523be8bc6c4fded
igordosreis <igorreis@gmail.com>
2024-01-05 10:35:40 -0300

    feat: add favoriteEstablishmentAddresses migration

---
+++ src/database/migrations/17-create-favorite_establishment_addresses.js
@@ -0,0 +1,46 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable strict */
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('favorite_establishment_addresses', {
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        unique: false,
+      },
+      establishment_addresses_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'establishments_addresses',
+          key: 'id', 
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+    }, {
+      underscored: true,
+      timestamps: false,
+      engine: 'InnoDB',
+      charset: 'latin1',
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('favorite_establishment_addresses');
+  },
+};
