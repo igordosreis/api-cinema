6cd0049ef1b46e3a45123909343215d0449bc8e8
igordosreis <igorreis@gmail.com>
2024-01-05 12:27:53 -0300

    feat: add toggleFavorite service

--- src/services/Favorites.service.ts
+++ src/services/Favorites.service.ts
@@ -1,1 +1,32 @@
-export default class FavoritesService {}
+/* eslint-disable max-lines-per-function */
+/* eslint-disable max-len */
+import { Op } from 'sequelize';
+import FavoriteEstablishmentAddresses from '../database/models/FavoriteEstablishmentAddresses.model';
+import { IFavoriteToggleRequest } from '../interfaces/IFavorites';
+import CustomError, { favoriteError } from '../utils/customError.util';
+
+export default class FavoritesService {
+  public static async toggleFavoriteEstablishment({
+    establishmentAddressId,
+    userId,
+  }: IFavoriteToggleRequest) {
+    try {
+      const [, created] = await FavoriteEstablishmentAddresses.findOrCreate({
+        where: {
+          [Op.and]: [{ establishmentAddressId }, { userId }],
+        },
+      });
+
+      const isEstablishmentAlreadyFavorite = !created;
+      if (isEstablishmentAlreadyFavorite) {
+        FavoriteEstablishmentAddresses.destroy({
+          where: {
+            [Op.and]: [{ establishmentAddressId }, { userId }],
+          },
+        });
+      }
+    } catch (error) {
+      throw new CustomError(favoriteError);
+    }
+  }
+}
