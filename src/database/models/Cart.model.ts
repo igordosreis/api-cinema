5f95a449a6a7e1836497243eae27f9a1b32ceb63
igordosreis <igorreis@gmail.com>
2024-01-12 15:58:08 -0300

    feat: add cartModel

---
+++ src/database/models/Cart.model.ts
@@ -0,0 +1,54 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import EstablishmentsAddressesModel from './EstablishmentsAddresses.model';
+
+class FavoriteEstablishmentAddresses extends Model {
+  declare userId: number;
+  declare establishmentAddressesId: string;
+  declare createdAt: Date;
+  declare updatedAt: Date;
+}
+
+FavoriteEstablishmentAddresses.init(
+  {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    addressId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'establishments_addresses',
+        key: 'id',
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      // defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      // defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    modelName: 'favorite_establishment_addresses',
+    sequelize: db,
+    underscored: true,
+    timestamps: false,
+    freezeTableName: true,
+  },
+);
+
+FavoriteEstablishmentAddresses.belongsTo(EstablishmentsAddressesModel, {
+  foreignKey: 'addressId',
+  as: 'favoriteEstablishmentAddress',
+});
+EstablishmentsAddressesModel.hasMany(FavoriteEstablishmentAddresses, {
+  foreignKey: 'addressId',
+  as: 'favoriteEstablishmentAddress',
+});
+
+export default FavoriteEstablishmentAddresses;
