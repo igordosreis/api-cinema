8397c2cea3ef59b3e241d89e850a5077196dbf6a
igordosreis <igorreis@gmail.com>
2024-01-19 12:29:45 -0300

    fix: default logo and cover

--- src/utils/formatImages.util.ts
+++ src/utils/formatImages.util.ts
@@ -9,10 +9,14 @@ export default class ImageFormatter {
   public static formatUrl({ imageName, folderPath = '' }: IImageFormat) {
     const isImageNameNotFound = !imageName;
     if (isImageNameNotFound) {
-      if (folderPath.includes('logo')) return 'default-logo-url';
-      if (folderPath.includes('cover')) return 'default-cover-url';
+      if (folderPath.includes('logo')) {
+        return `${SUPERAPP_CINEMA_BASE_URL}/images/logo/logo_default.png`;
+      }
+      if (folderPath.includes('cover')) {
+        return `${SUPERAPP_CINEMA_BASE_URL}/images/cover/cover_default.png`;
+      }
     }
- 
+
     const imageUrl = `${SUPERAPP_CINEMA_BASE_URL}/images${folderPath}/${imageName}`;
 
     return imageUrl;
