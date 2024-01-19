7eef930b1fb3fb237f07bf10321f0c0171c19760
igordosreis <igorreis@gmail.com>
2024-01-19 16:15:48 -0300

    feat: add tags table

---
+++ src/database/migrations/19-create-tags.js
@@ -0,0 +1,38 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable strict */
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('tags', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+    }, {
+      underscored: true,
+      timestamps: false,
+      engine: 'InnoDB',
+      charset: 'latin1',
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('tags');
+  },
+};
