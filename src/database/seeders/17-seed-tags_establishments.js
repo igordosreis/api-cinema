156c44bd847f45b0575ebcc57c52f1c07454dc68
igordosreis <igorreis@gmail.com>
2024-01-22 16:35:52 -0300

    feat: add tagsEstablishments seeder

---
+++ src/database/seeders/17-seed-tags_establishments.js
@@ -0,0 +1,86 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable max-len */
+/* eslint-disable max-lines-per-function */
+
+module.exports = {
+  up: async (QueryInterface) => {
+    await QueryInterface.bulkInsert('tags_establishments', [
+      {
+        tag_id: 0,
+        establishment_id: 753,
+      },
+      {
+        tag_id: 1,
+        establishment_id: 753,
+      },
+      {
+        tag_id: 2,
+        establishment_id: 753,
+      },
+      {
+        tag_id: 3,
+        establishment_id: 753,
+      },
+      {
+        tag_id: 4,
+        establishment_id: 753,
+      },
+      {
+        tag_id: 5,
+        establishment_id: 753,
+      },
+      {
+        tag_id: 6,
+        establishment_id: 753,
+      },
+      {
+        tag_id: 7,
+        establishment_id: 753,
+      },
+      {
+        tag_id: 0,
+        establishment_id: 488,
+      },
+      {
+        tag_id: 1,
+        establishment_id: 488,
+      },
+      {
+        tag_id: 2,
+        establishment_id: 488,
+      },
+      {
+        tag_id: 3,
+        establishment_id: 488,
+      },
+      {
+        tag_id: 4,
+        establishment_id: 488,
+      },
+      {
+        tag_id: 5,
+        establishment_id: 488,
+      },
+      {
+        tag_id: 0,
+        establishment_id: 3543,
+      },
+      {
+        tag_id: 2,
+        establishment_id: 3543,
+      },
+      {
+        tag_id: 3,
+        establishment_id: 3543,
+      },
+      {
+        tag_id: 4,
+        establishment_id: 3543,
+      },
+    ], {});
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('tags_establishments', null, {});
+  },
+};
