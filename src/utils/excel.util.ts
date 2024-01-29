27028a9a14bee395719da1acdae7f11607cfb028
igordosreis <igorreis@gmail.com>
2024-01-29 12:30:54 -0300

    feat: add excel util

---
+++ src/utils/excel.util.ts
@@ -0,0 +1,13 @@
+import xlsx from 'xlsx';
+
+export default class Excel {
+  static read<T>(buffer: Buffer): Array<T> {
+    const workbook = xlsx.read(buffer, { type: 'buffer' });
+    const sheetName = workbook.SheetNames[0];
+    const worksheet = workbook.Sheets[sheetName];
+
+    const data = xlsx.utils.sheet_to_json(worksheet) as Array<T>;
+
+    return data;
+  }
+}
