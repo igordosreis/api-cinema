8bef278aa42d3cc7205eaa7bfe1b1916866b4ec8
Igor dos Reis <igorreis@gmail.com>
2024-02-14 16:24:48 -0300

    fix: vouchersUser seeder

--- src/database/seeders/08-seed-vouchers_user.js
+++ src/database/seeders/08-seed-vouchers_user.js
@@ -53,6 +53,7 @@ module.exports = {
       id: index + 1,
       voucher_code: generateUniqueVoucher(),
       product_id: Math.floor(Math.random() * 49) + 1, // Random number between 1 and 50
+      establishment_id: 488,
       // order_id: Math.floor(Math.random() * 10) + 1, // Random number between 1 and 10000
       expire_at: generateRandomDate(2024, 2025),
       // payment_id: generateUniquePaymentId(),
