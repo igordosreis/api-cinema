748a29a7f109f8621f6f479e4261a6209ffd6f94
igordosreis <igorreis@gmail.com>
2024-02-15 16:22:12 -0300

    feat: add editPack route

--- src/routes/dashboard.routes.ts
+++ src/routes/dashboard.routes.ts
@@ -5,10 +5,11 @@ const dashboardRouter = Router();
 
 dashboardRouter.post('/product/create', DashboardController.createProduct);
 dashboardRouter.patch('/product/edit', DashboardController.editProduct);
+dashboardRouter.post('/pack/create', DashboardController.createPack);
+dashboardRouter.patch('/pack/edit', DashboardController.editPack);
 dashboardRouter.post('/voucher/create', DashboardController.createVoucher);
 dashboardRouter.get('/voucher/get', DashboardController.getVouchers);
 dashboardRouter.put('/voucher/withdraw/single', DashboardController.withdrawSingleVoucher);
-dashboardRouter.post('/pack/create', DashboardController.createPack);
 dashboardRouter.post('/tag/create', DashboardController.createTag);
 
 export default dashboardRouter;
