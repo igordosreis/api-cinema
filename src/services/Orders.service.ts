5d4e75e0da585587e748417914d301a9e7483f5b
Igor dos Reis <igorreis@gmail.com>
2024-02-27 17:36:40 -0300

    feat: sort orders by nearest date

--- src/services/Orders.service.ts
+++ src/services/Orders.service.ts
@@ -371,6 +371,7 @@ export default class OrdersService {
             [Op.lte]: lastDay,
           },
         },
+        order: [['updatedAt', 'ASC']],
         limit,
         offset: page * limit,
       }) as IOrderAll[];
