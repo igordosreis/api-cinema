47b31fe5582b515e092f8abc955b90407f80d7ed
igordosreis <igorreis@gmail.com>
2024-03-11 15:23:41 -0300

    feat: add batches table migration

---
+++ src/database/migrations/25-create-batches.js
@@ -0,0 +1,62 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable strict */
+
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('batches', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      batch_code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'establishments_products',
+          key: 'product_id', 
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      establishment_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'establishments',
+          key: 'id', 
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      expire_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+    }, {
+      engine: 'InnoDB',
+      charset: 'latin1',
+      underscored: true,
+      timestamps: false,
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('batches');
+  },
+};
