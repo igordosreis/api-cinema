4c267353833bface15cf4f6242a1ff99ec9b6c97
igordosreis <igorreis@gmail.com>
2024-03-15 12:49:06 -0300

    feat: add commentlogs and commentactions models

---
+++ src/database/models/CommentActions.model.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class CommentActions extends Model {
+  declare id: number;
+  declare name: string;
+  declare urlPath: string;
+  declare httpMethod: string;
+  declare createdAt: Date;
+  declare updatedAt: Date;
+}
+
+CommentActions.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      // defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      // defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    modelName: 'comment_actions',
+    sequelize: db,
+    underscored: true,
+    timestamps: false,
+    freezeTableName: true,
+  },
+);
+
+export default CommentActions;
---
+++ src/database/models/CommentLogs.model.ts
@@ -0,0 +1,62 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import CommentActions from './CommentActions.model';
+
+class CommentLogs extends Model {
+  declare id: number;
+  declare comment: string;
+  declare originalUrl: string;
+  declare userId: number;
+  declare actionId: number;
+  declare createdAt: Date;
+  declare updatedAt: Date;
+}
+
+CommentLogs.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    comment: {
+      type: DataTypes.STRING,
+    },
+    originalUrl: {
+      type: DataTypes.STRING,
+    },
+    userId: {
+      type: DataTypes.NUMBER,
+    },
+    actionId: {
+      type: DataTypes.NUMBER,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      // defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      // defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    modelName: 'comment_logs',
+    sequelize: db,
+    underscored: true,
+    timestamps: false,
+    freezeTableName: true,
+  },
+);
+
+CommentLogs.belongsTo(CommentActions, {
+  foreignKey: 'actionId',
+  as: 'commentAction',
+});
+CommentActions.hasMany(CommentLogs, {
+  foreignKey: 'actionId',
+  as: 'commentAction',
+});
+
+export default CommentLogs;
