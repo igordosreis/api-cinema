a1d00abe10918c36d8d93d9954848f1c95080bc7
igordosreis <igorreis@gmail.com>
2024-03-15 14:55:07 -0300

    fix: interface IAdmin and IComments

--- src/interfaces/IAdmin.ts
+++ src/interfaces/IAdmin.ts
@@ -1,4 +1,5 @@
 export interface IAdminInfo {
+  id: number;
   name: string;
   email: string;
 }
---
+++ src/interfaces/IComments.ts
@@ -0,0 +1,6 @@
+import { IAdminInfo } from './IAdmin';
+
+export interface ICommentsInBody {
+  comment: string;
+  userInfo: IAdminInfo;
+}
