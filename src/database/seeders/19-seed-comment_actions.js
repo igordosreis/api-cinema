3b1dbc293439107bcf6897feefac4d88db8829a3
igordosreis <igorreis@gmail.com>
2024-03-15 16:28:55 -0300

    feat: add comment_actions seeder

---
+++ src/database/seeders/19-seed-comment_actions.js
@@ -0,0 +1,59 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+/* eslint-disable max-len */
+/* eslint-disable max-lines-per-function */
+
+module.exports = {
+  up: async (QueryInterface) => {
+    await QueryInterface.bulkInsert('comment_actions', [
+      {
+        name: 'edição de rede',
+        url_path: '/establishment/brand/edit',
+        http_method: 'PATCH',
+      },
+      {
+        name: 'edição de imagem de rede',
+        url_path: '/establishment/image/edit',
+        http_method: 'PUT',
+      },
+      {
+        name: 'criação de produto',
+        url_path: '/shop/product/create',
+        http_method: 'POST',
+      },
+      {
+        name: 'edição de produto',
+        url_path: '/shop/product/edit',
+        http_method: 'PATCH',
+      },
+      {
+        name: 'criação de pacote',
+        url_path: '/shop/pack/create',
+        http_method: 'POST',
+      },
+      {
+        name: 'edição de pacote',
+        url_path: '/shop/pack/edit',
+        http_method: 'PATCH',
+      },
+      {
+        name: 'criação de voucher',
+        url_path: '/voucher/create',
+        http_method: 'POST',
+      },
+      {
+        name: 'remoção de voucher',
+        url_path: '/voucher/withdraw/single',
+        http_method: 'PUT',
+      },
+      {
+        name: 'criação de tag',
+        url_path: '/tag/create',
+        http_method: 'POST',
+      },
+    ], {});
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete('comment_actions', null, {});
+  },
+};
