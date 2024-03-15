998255fd85411220d5b1eac21a7a6fa653dc2e94
igordosreis <igorreis@gmail.com>
2024-03-15 12:17:45 -0300

    feat: add comment_logs migration

--- src/database/migrations/26-create-comment_logs.js
+++ src/database/migrations/26-create-comment_logs.js
@@ -5,24 +5,34 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('comment_actions', {
+    await queryInterface.createTable('comment_logs', {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false,
       },
-      name: {
+      comment: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      url_path: {
+      original_url: {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      http_method: {
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      action_id: {
         type: Sequelize.STRING,
         allowNull: false,
+        references: {
+          model: 'comment_actions',
+          key: 'id', 
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
       },
       created_at: {
         type: Sequelize.DATE,
@@ -41,6 +51,6 @@ module.exports = {
   },
 
   down: async (queryInterface) => {
-    await queryInterface.dropTable('comment_actions');
+    await queryInterface.dropTable('comment_logs');
   },
 };
