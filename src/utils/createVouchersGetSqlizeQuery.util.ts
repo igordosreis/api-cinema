d9bd5ef9d949cd2cba75a46bef2ee14b3ba80e9b
Igor dos Reis <igorreis@gmail.com>
2024-07-19 18:59:34 -0300

    feat: expand search fields in voucher get

--- src/utils/createVouchersGetSqlizeQuery.util.ts
+++ src/utils/createVouchersGetSqlizeQuery.util.ts
@@ -5,11 +5,73 @@ import { Op } from 'sequelize';
 import { IVouchersGetDashboardParsed } from '../interfaces/IVouchers';
 
 class CreateVouchersGetSqlizeQuery {
-  private addParams = ({ productId, establishmentId, search }: IVouchersGetDashboardParsed) => {
+  private getVoucherTypeReturn = ({
+    search,
+    voucherType,
+  }: IVouchersGetDashboardParsed) => {
+    if (search && (voucherType === 'available' || voucherType === undefined)) {
+      const obj = { 
+        [Op.or]: {
+          voucherCode: { [Op.substring]: search },
+          '$brand.name$': { [Op.substring]: search },
+          '$vouchersAvailable.name$': { [Op.substring]: search },
+        },
+      };
+      
+      return obj;
+    }
+
+    if (search && (voucherType === 'user')) {
+      const obj = { 
+        [Op.or]: {
+          voucherCode: { [Op.substring]: search },
+          '$brand.name$': { [Op.substring]: search },
+          '$productVoucherInfo.name$': { [Op.substring]: search },
+        },
+      };
+
+      return obj;
+    }
+
+    if (search && (voucherType === 'withdraw')) {
+      const obj = { 
+        [Op.or]: {
+          voucherCode: { [Op.substring]: search },
+          '$brand.name$': { [Op.substring]: search },
+          '$vouchersWithdraw.name$': { [Op.substring]: search },
+        },
+      };
+
+      return obj;
+    }
+  };
+
+  private addParams = (params: IVouchersGetDashboardParsed) => {
+    const {
+      productId,
+      establishmentId,
+      search,
+    } = params;
+
     const searchQuery = [];
     if (productId) searchQuery.push({ productId });
     if (establishmentId) searchQuery.push({ establishmentId });
-    if (search) searchQuery.push({ voucherCode: search });
+    if (search) {
+      const isSearchNumber = Number(search);
+      if (isSearchNumber) {
+        const parsedSearch = Number(search);
+        const parsedProductId = productId || parsedSearch;
+        const parsedEstablishmentId = establishmentId || parsedSearch;
+        searchQuery.push({ 
+          [Op.or]: {
+            productId: parsedProductId,
+            establishmentId: parsedEstablishmentId,
+          },
+        });
+      } else {
+        searchQuery.push(this.getVoucherTypeReturn(params));
+      }
+    }
 
     return {
       where: {
